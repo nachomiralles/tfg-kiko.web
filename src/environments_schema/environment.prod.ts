34fa44060ff4b4ba2c518c79cf3b607cf66e09db
Nacho Miralles <nachomiralles@gmail.com>
2018-07-19 10:35:25 +0200

    Environments Schema added

---
+++ src/environments_schema/environment.prod.ts
@@ -0,0 +1,12 @@
+// UPDTE WITH YOUR APPKEY
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  }
+};
